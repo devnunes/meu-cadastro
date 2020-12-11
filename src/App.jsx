dd33270136f3a9a0114ab39edeaedd70890f6b1c
mnunes <matheusnunesinfo@gmail.com>
2020-12-11 09:43:29 -0300

    url-polyfill teste

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
+import 'url-polyfill';
+
 function App() {
   return (
     <>
